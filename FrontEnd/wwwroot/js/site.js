801757626a4e02804362f56d3423582494fa5c25
Moomosa <113059275+Moomosa@users.noreply.github.com>
2023-09-08 09:11:39 -0600

    Added links from front page tables to their respective index page. Altered type index pages to be mobile friendly and responsive. Altered default font for entire project.

--- FrontEnd/wwwroot/js/site.js
+++ FrontEnd/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", function () {
+    var tableRows = document.querySelectorAll(".table-row-clickable");
+    tableRows.forEach(function (row) {
+        row.addEventListener("click", function () {
+            var href = row.getAttribute("data-href");
+            if (href) {
+                window.location.href = href;
+            }
+        });
+    });
+});
